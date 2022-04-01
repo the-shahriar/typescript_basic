824697bf39cbcd32ee3659f1df4de260605495bc
Shahriar Islam <theshahriarislam@gmail.com>
2022-04-01 20:54:42 +0600

    interface, type, generic

--- index.js
+++ index.js
@@ -19,3 +19,36 @@ var substract = function (num1, num2) {
 };
 console.log(add(5, 3));
 console.log(substract(5, 3));
+// array declarations
+// generic array
+var array = [12, 133, "Tamim"];
+// normal
+var arr = [12, 133, 454];
+var arr2 = ["odif", "vvv", "jggfd"];
+var id = 122;
+// examples
+var person = {
+    name: "Tamim",
+    age: 22,
+    hobby: "Programming"
+};
+var person2 = {
+    name: "Mezba",
+    age: 25
+};
+/* --------------enums------------- */
+var Color;
+(function (Color) {
+    Color["color1"] = "#ededed";
+    Color["color2"] = "#eededh";
+    Color["color3"] = "#dddddd";
+})(Color || (Color = {}));
+console.log(Color.color2);
+/* ------------Generic-------------- */
+// to declare type when function called
+function _concat(a, b) {
+    return a + b;
+}
+_concat(1, 3);
+_concat("Shahriar", "Islam");
+_concat("Shahriar", "Islam");
--- index.ts
+++ index.ts
@@ -26,3 +26,58 @@ const substract = (num1: number, num2: number): number => {
 
 console.log(add(5, 3));
 console.log(substract(5, 3));
+
+// array declarations
+
+// generic array
+const array: Array<number | string> = [12, 133, "Tamim"];
+
+// normal
+const arr: number[] = [12, 133, 454];
+const arr2: string[] = ["odif", "vvv", "jggfd"];
+
+// type notation
+type numberAndString = number | string;
+const id: numberAndString = 122;
+
+/* -------Interface-------------  */
+interface IPerson {
+  name: string;
+  age: number;
+  hobby?: string;
+}
+
+// examples
+const person: IPerson = {
+  name: "Tamim",
+  age: 22,
+  hobby: "Programming",
+};
+
+const person2: IPerson = {
+  name: "Mezba",
+  age: 25,
+};
+
+/* --------------enums------------- */
+enum Color {
+  color1 = "#ededed",
+  color2 = "#eededh",
+  color3 = "#dddddd",
+}
+
+console.log(Color.color2);
+
+/* ------------Generic-------------- */
+// to declare type when function called
+
+function _concat<T extends string | number>(
+  a: T,
+  b: T
+): T extends string ? string : number {
+  return <any>a + <any>b;
+}
+
+_concat(1, 3);
+_concat("Shahriar", "Islam");
+_concat("Shahriar", "Islam");
